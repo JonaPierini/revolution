b87929dd004fe1d51286f0ce945953042eca98e4
Martin Ballester <martinballester1@gmail.com>
2024-01-13 15:27:58 -0300

    fixex

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -17,11 +17,13 @@ export const ProductItem = ({ product }) => {
       style={{ width: "18rem", margin: "10px" }}
     >
       <img src={product.image} className="card-img-top" alt={product.name} />
-      <div className="card-body-products">
-        <h5 className="card-title">{product.name}</h5>
-        <button className="expand-btn" onClick={handleNavigate}>
-          View more
-        </button>
+      <div className="card-body">
+        <div className="card-body-products">
+          <h5 className="card-title">{product.name}</h5>
+          <button className="expand-btn" onClick={handleNavigate}>
+            View more
+          </button>
+        </div>
       </div>
     </div>
   );
